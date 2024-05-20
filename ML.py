95d086b24a4d2fbf694e124dad24d7e9735d3784
JustAStudentAI <132246011+JustAStudentAI@users.noreply.github.com>
2024-05-20 16:03:05 -0400

    ML.py

--- ML.py
+++ ML.py
@@ -83,6 +83,22 @@ def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
     b2 -= alpha * np.reshape(db2, (10, 1))
     return W1, b1, W2, b2
 
+def gradient_descent(X, Y, alpha, iterations):
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        
+        # print every 20th iteration
+        if i % 20 == 0:
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
+
 def make_predictions(X, W1, b1, W2, b2):
     _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
     predictions = get_predictions(A2)
@@ -107,30 +123,10 @@ def get_accuracy(predictions, Y):
     print(predictions, Y)
     return np.sum(predictions == Y) / Y.size
 
-def gradient_descent(X, Y, alpha, iterations):
-    W1, b1, W2, b2 = init_params()
-    for i in range(iterations):
-        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
-        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
-        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
-        
-        # print every 20th iteration
-        if i % 20 == 0:
-            print("Iteration: ", i)
-            predictions = get_predictions(A2)
-            print(get_accuracy(predictions, Y))
-    return W1, b1, W2, b2
-
-W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
-
-
-
-
 def make_predictions(X, W1, b1, W2, b2):
     _, _, _, A2 = forward_prop(W1, b1, W2, b2, X) 
     predictions = get_predictions(A2) 
     return predictions
-
 
 def test_prediction(index, W1, b1, W2, b2): 
     current_image = X_train[:, index, None] 
